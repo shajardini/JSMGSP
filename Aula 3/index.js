6c01d556e05da9b3cfec5b92842d43b30c7d0baf
shajardini <sha.jardini@gmail.com>
2023-05-04 15:09:37 -0300

    Atualização Título

--- Aula 3/index.js
+++ Aula 3/index.js
@@ -1,1 +1,12 @@
-prompt('Qual seu nome?')
+// var nome = prompt('Qual seu nome?')
+
+// alert('Boa tarde '+ nome+'!')//+ ${}
+
+// alert(`Seja bem vindo(a) ${nome}`)/*utilizando template string, marcação é feita com crase*/
+
+//declarar variável - var, let, const
+
+//Tipos primitivos de dados
+
+var pi = 3.1415
+console.log(pi)
