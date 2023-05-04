320abd983ed4f71e5576172c00940bc86be59a6d
shajardini <sha.jardini@gmail.com>
2023-05-04 12:16:16 -0300

    first commit

---
+++ Aula 1/index.js
@@ -0,0 +1,7 @@
+document.getElementById('teste1').innerText='Oi, Sharlayne!'
+
+document.getElementById('teste1').style.color='red'
+
+document.getElementById('teste1').style.fontSize='30px'
+
+//font-size
---
+++ Aula 2/index.js
@@ -0,0 +1,26 @@
+//Recursos nativos JavaScript
+//Caixas diálogos
+
+alert('Bem vindo pessoal!')//Janela de alerta
+
+
+confirm('Vocês estão entendendo?')//janela de confirmação
+
+prompt('Qual seu nome?')//janela para obter algum dado/informação do usuário
+
+//Mensagens para o console
+
+console.log("Boa tarde pessoal!")//imprime texto no console
+
+console.info('Hoje é quarta-feira.')
+
+console.warn('Essa é uma janela de alerta!')
+
+console.error('Usuário não definido.')
+
+
+
+
+
+
+
