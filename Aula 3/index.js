cb7956ba214f63097ed245d9f673b5ad06c920a8
shajardini <sha.jardini@gmail.com>
2023-05-04 15:37:09 -0300

    Atualização Título

--- Aula 3/index.js
+++ Aula 3/index.js
@@ -8,5 +8,12 @@
 
 //Tipos primitivos de dados
 
-var pi = 3.1415
-console.log(pi)
+var pi = 3.1415//casas decimais separadas pelo .
+console.log('Tipo de dado da variável pi: ' + typeof pi)//number
+
+var texto = 'Olá pessoal!'
+console.log('Tipo de dados da variável texto: ' + typeof texto)//string - tipo texto
+console.log(texto)
+
+texto = 'A Nadmila é uma aluna maravilhosa!'
+console.log(texto)
