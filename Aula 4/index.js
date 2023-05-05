11463f6b6e9b6400759ca151e8ce3f4d8bbfd7fb
shajardini <sha.jardini@gmail.com>
2023-05-05 14:37:35 -0300

    Atualização Título

--- Aula 4/index.js
+++ Aula 4/index.js
@@ -1,2 +1,15 @@
 //variável - let, var, const
 // executar no console--> f8 ou fn+f8
+
+let n1 = 10
+let n2= 5
+
+let soma = n1+n2// 15
+let subtracao = n1-n2// 5
+let divisao = n1/n2//2
+let multiplicacao = n1*n2//50
+
+console.log(soma)
+console.log(subtracao)
+console.log(divisao)
+console.log(multiplicacao)
