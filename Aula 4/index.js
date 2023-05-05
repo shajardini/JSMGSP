a35d36411144727b569600eb9821fa2794ac94e4
shajardini <sha.jardini@gmail.com>
2023-05-05 14:56:47 -0300

    Atualização Título

--- Aula 4/index.js
+++ Aula 4/index.js
@@ -8,8 +8,15 @@ let soma = n1+n2// 15
 let subtracao = n1-n2// 5
 let divisao = n1/n2//2
 let multiplicacao = n1*n2//50
+let resto = n1 % n2 //0
+let exponenciacao = n1 ** n2//100000
 
-console.log(soma)
-console.log(subtracao)
-console.log(divisao)
-console.log(multiplicacao)
+
+// console.log(soma)
+// console.log(subtracao)
+// console.log(divisao)
+// console.log(multiplicacao)
+// console.log(resto)
+// console.log(exponenciacao)
+
+console.log(`Soma: ${soma} \nSubtração: ${subtracao} \nDivisão: ${divisao} \nMultiplicação: ${multiplicacao} \nResto da divisão: ${resto} \nExponenciação: ${exponenciacao}`)
