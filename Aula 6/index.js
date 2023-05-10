56d6b8bdcf343698c491ab3041826d6279899ef2
shajardini <sha.jardini@gmail.com>
2023-05-10 14:51:14 -0300

    atualização

--- Aula 6/index.js
+++ Aula 6/index.js
@@ -1,3 +1,20 @@
 function entradaDoMouse(){
     console.log('Entrada do mouse, chamando por função!')
 }
+
+function mudancaValor(){
+    console.log('Valor alterado')
+   
+}
+
+function eventoFoco(){
+    console.log('Foco!')
+}
+
+function saidaFoco(){
+    alert('Saiu do foco.')
+}
+
+function clique(){
+    confirm('Tem certeza que deseja realizar essa ação?')
+}
