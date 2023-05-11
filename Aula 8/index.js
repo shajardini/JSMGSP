bf1d054e30893d7fac13936c66195ba9ee45a844
shajardini <sha.jardini@gmail.com>
2023-05-11 17:06:37 -0300

    atualização

--- Aula 8/index.js
+++ Aula 8/index.js
@@ -1,57 +1,78 @@
-//logica?true:false
+// //logica?true:false
 
-//Estrutura de condição
-//if/
-//if/else
-//if/ else if/else
+// //Estrutura de condição
+// //if/
+// //if/else
+// //if/ else if/else
 
-var velocidade = 70
-//if
-//if(teste){}
-if(velocidade>100){
-    console.log('Você foi multado!')
-}
-
-console.log('Continuação do código após a estrutura if simples')
-
-//if/else
-
-/* (se/senão)
-if(teste){
-    true/
-}else{
-    false
-}
-*/ 
-
-var nota = 4
-    //true ou false
-if(nota>6){
-    console.log('Aprovado')
-}else{
-    console.log('Reprovado')
-}
-
-//aprovado, reprovado, exame
-
-// //if(teste){
-
-// }else if(teste){
-
-// }else{
-
+// var velocidade = 70
+// //if
+// //if(teste){}
+// if(velocidade>100){
+//     console.log('Você foi multado!')
 // }
 
-var nota1=5
-//5  false
-if(nota1>7){
-    console.log('Aprovado')
-}else if(nota1>5){
-    console.log('Exame')
-}else{
-    console.log('Reprovado')
+// console.log('Continuação do código após a estrutura if simples')
+
+// //if/else
+
+// /* (se/senão)
+// if(teste){
+//     true/
+// }else{
+//     false
+// }
+// */ 
+
+// var nota = 4
+//     //true ou false
+// if(nota>6){
+//     console.log('Aprovado')
+// }else{
+//     console.log('Reprovado')
+// }
+
+// //aprovado, reprovado, exame
+
+// // //if(teste){
+
+// // }else if(teste){
+
+// // }else{
+
+// // }
+
+// var nota1=5
+// //5  false
+// if(nota1>7){
+//     console.log('Aprovado')
+// }else if(nota1>5){
+//     console.log('Exame')
+// }else{
+//     console.log('Reprovado')
+// }
+
+// //
+
+
+function verificar(){
+    let timea = Number(document.getElementById('timea').value)//5
+    let timeb = Number(document.getElementById('timeb').value)//2
+
+    let area = document.getElementById('resultado')
+
+    //se(oquetemdentrodotimea for maior oquetemdentrodotimeb)
+
+    if(timea>timeb){
+        //selecionando a area e add um texto
+       area.innerText= 'Time A vencedor.'
+
+       //senão, se(oquetemdentrodotimea for menor que o temdentro do time b)
+    }else if(timea<timeb){
+        area.innerText='Time B vencedor.'
+
+        //senão
+    }else{
+        area.innerText='Empate!'
+    }
 }
-
-//
-
-
---
+++ Aula 9/index.js
@@ -0,0 +1,17 @@
+//estrutura - swtich
+
+var letra = 'a'
+
+switch(letra){//a
+    case 'a':
+        console.log('Letra A')
+       break
+
+    case 'b':
+        console.log('Letra B')
+        break
+        
+    default:
+        console.log('Não faz parte das letras selecionadas.')
+
+}
