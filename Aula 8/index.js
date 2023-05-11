8884dae291236876c703ea20c451413b09434e2f
shajardini <sha.jardini@gmail.com>
2023-05-11 15:25:05 -0300

    atualização

---
+++ Aula 8/index.js
@@ -0,0 +1,33 @@
+//logica?true:false
+
+//Estrutura de condição
+//if/
+//if/else
+//if/ else if/else
+
+var velocidade = 70
+//if
+//if(teste){}
+if(velocidade>100){
+    console.log('Você foi multado!')
+}
+
+console.log('Continuação do código após a estrutura if simples')
+
+//if/else
+
+/* (se/senão)
+if(teste){
+    true/
+}else{
+    false
+}
+*/ 
+
+var nota = 4
+    //true ou false
+if(nota>6){
+    console.log('Aprovado')
+}else{
+    console.log('Reprovado')
+}
