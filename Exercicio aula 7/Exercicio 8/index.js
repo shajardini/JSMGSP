f1eed5ef2c4f5a6bec3e1cd3d682736546eaad09
shajardini <sha.jardini@gmail.com>
2023-05-11 15:02:56 -0300

    atualização

--- Exercicio aula 7/Exercicio 8/index.js
+++ Exercicio aula 7/Exercicio 8/index.js
@@ -1,3 +1,11 @@
 function somar(){
-    
+   var n1 = Number(document.getElementById('number1').value)//6
+   var n2 = Number(document.getElementById('number2').value)//85
+   var area = document.getElementById('resultado')//seleção do elemento que vai exibir o resultado
+
+   var soma = n1 + n2//realizamos a soma
+
+   area.innerText=soma//exibimos dentro da area o resultado
+
+
 }
