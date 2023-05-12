47498cc6f1b21decdb32179ffff894bc953830c2
shajardini <sha.jardini@gmail.com>
2023-05-12 15:49:29 -0300

    atualização

---
+++ Aula 11/index.js
@@ -0,0 +1,40 @@
+//Estrutura de repetição - while
+
+// let contador = 5
+//         //true
+
+//         //1 2
+// while(contador<=5){//1 - true/
+//     console.log('Oi')
+//     contador++
+//     //contador = contador + 1
+// }
+
+// let velocidade = 80
+
+// while(velocidade>0){//80-70-60-
+//     console.log(velocidade)
+//     velocidade = velocidade - 10//velocidade-=10
+
+// }
+// console.log('velocidade:' + velocidade)
+// console.log('Oi')
+
+//do...While
+
+
+
+// do{ 
+
+// }while()
+
+//faça{
+//     isso
+// }enquanto(fortrue)
+
+let velo = 0
+do{
+    console.log(velo)
+    velo = velo - 10//velocidade-=10 -10
+}while(velo>0)//false
+//-10
---
+++ Aula 11/node_4ad307087fd28.tmp.js
@@ -0,0 +1,10 @@
+//Estrutura de repetição - while
+
+let contador = 5
+        //true
+
+        //1 2
+while(contador<=5){//1 - true/
+    console.log('Oi')
+    //contador = contador + 1
+}
