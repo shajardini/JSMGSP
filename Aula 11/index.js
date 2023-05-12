646e4048c8af62480e4af99f5f80ad6c3c796af0
shajardini <sha.jardini@gmail.com>
2023-05-12 16:35:19 -0300

    atualização

--- Aula 11/index.js
+++ Aula 11/index.js
@@ -20,21 +20,42 @@
 // console.log('velocidade:' + velocidade)
 // console.log('Oi')
 
-//do...While
+//Laço de repetição - do...While
 
+//Estrutura:
 
+/*do{ 
 
-// do{ 
+}while()*/
 
-// }while()
+/*Traduzindo
 
-//faça{
-//     isso
-// }enquanto(fortrue)
+faça{
+    isso
+}enquanto(fortrue)*/
 
-let velo = 0
+// let velo = 0
+// do{
+//     console.log(velo)
+//     velo = velo - 10//velocidade-=10 -10
+// }while(velo>0)//false
+// //-10
+
+let numero = 0
+
+while(numero<=100){
+   
+    document.getElementById('resultado').innerHTML+=`<br>${numero}`
+    numero++// numero = numero + 1
+}
+
+// do ... while
+
+let n = 0
 do{
-    console.log(velo)
-    velo = velo - 10//velocidade-=10 -10
-}while(velo>0)//false
-//-10
+    document.getElementById('resultado').innerHTML+=`<br>${n}`
+    n++// numero = numero + 1
+}while(n<=100)//101
+
+console.log('O que tem dentro da variável número agora? '+ numero )
+console.log('O que tem dentro da variável nagora? '+ n )
