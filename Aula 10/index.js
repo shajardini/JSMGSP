1a7e88742e5c01d17a09acf92711e0fa57a553f1
shajardini <sha.jardini@gmail.com>
2023-05-12 14:37:51 -0300

    atualização

---
+++ Aula 10/index.js
@@ -0,0 +1,35 @@
+function calcular() {
+    let n1 = Number(document.getElementById('numero1').value)
+    let n2 = Number(document.getElementById('numero2').value)
+    let operacao = document.getElementById('selectop').value
+    let area=document.getElementById('resultado')
+
+    let resultado = 0
+
+
+    // //switch(teste){
+    //     case:
+    //     façaisso
+    //     break
+
+    //     cade tatata:
+    //     façaisso
+    //     break
+    // }
+
+    switch(operacao){
+        case '+':
+            resultado = n1+n2
+            break
+        case '-':
+            resultado = n1-n2
+            break
+        case '/':
+            resultado = n1 / n2
+            break
+        case '*':
+            resultado = n1 * n2
+    }
+
+    
+}
