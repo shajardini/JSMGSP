0fa9ac9060927957c330cd8ef72b040698ec5ef6
shajardini <sha.jardini@gmail.com>
2023-05-16 16:26:56 -0300

    atualização

--- Aula 13/index.js
+++ Aula 13/index.js
@@ -1,5 +1,23 @@
 var nome = 'Shay'//simples
-
+        //   0        1      2        3
 var nomes = ['Ana', 'Luan', 'Pablo', 'Pedro', 'Nadmila', 'Lilyan']//array[]
 
 console.log(nomes)
+
+console.log(nomes[4])//mostrar dado em uma posição específica
+
+nomes[0]='Giovanna'//substituiu a Ana pela Giovanna
+console.log(nomes)
+
+nomes.push('Mariana')//adiciona na última posição
+console.log(nomes)
+
+nomes.splice(4, 0, 'Atena')//inserindo elemento(posiçãoparainserir,0,"novo elemento")
+console.log(nomes)
+
+nomes.splice(1,1)//removendo da lista
+console.log(nomes)
+
+console.log(nomes.length)//mostra o tamanho do array
+
+console.log(nomes.sort())//ajustar em ordem crescente
